a1e46589b755c6e72d741c69a27816dfb68e9c6a
Munoz Matéo <mateomunoz@orange.fr>
2022-03-22 12:47:00 +0100

    classe Personnage fini

--- src/Personnage.cpp
+++ src/Personnage.cpp
@@ -1,5 +1,6 @@
 #include "Personnage.h"
 #include <assert.h>
+#include <iostream>
 
 using namespace std;
 
@@ -8,7 +9,7 @@ Personnage::Personnage(){
     position.y=0;
     projectile=NULL;
     direction={0,0};
-    vitesse=0;
+    vitesse=1;
     nom="p0";
     enVie=true;
 }
@@ -36,8 +37,40 @@ vector<float> Personnage::getDir() const{
     return direction;
 }
 
-void Personnage::setDir(vector<float> d){
-    direction=d;
+float Personnage::getVit() const{
+    return vitesse;
+}
+
+void Personnage::setVit(float v){
+    vitesse=v;
+}
+
+void Personnage::deplacerD(){
+    direction={1*vitesse,0};
+    position.x=position.x+direction.at(0);
+}
+
+void Personnage::deplacerG(){
+    direction={-1*vitesse,0};
+    position.x=position.x+direction.at(0);
+}
+
+void Personnage::saut(){
+    direction={0,1*vitesse};
+    position.y=position.y+direction.at(1);
+}
+
+void Personnage::tombe(){
+    direction={0,-1*vitesse};
+    position.y=position.y+direction.at(1);
+}
+
+const string& Personnage::getNom() const{
+    return nom;
+}
+
+void Personnage::setNom(const string nomP){
+    nom = nomP;
 }
 
 
--- src/Personnage.h
+++ src/Personnage.h
@@ -19,22 +19,23 @@ class Personnage
         vector<float> direction; /*! \brief le vecteur de direction du personnage*/
         float vitesse;/*! \brief la vitesse du personnage*/
         string nom;/*! \brief le nom du personnage*/
-        bool enVie;/*! \brief info si le personnage est en vie ou non*/
+       
     public:
+        bool enVie;/*! \brief info si le personnage est en vie ou non*/
         Personnage();/*! \brief position de base : 0,0; projectile à NULL; direction, vitesse à 0, nom à "p0", enVie à true*/
         ~Personnage();/*! \brief delete des projectiles, tout à 0(pos x,y; direction; vitesse), nom à NULL, enVie à NULL*/
         pos getPos() const;/*! \brief renvoie la position en x et en y du personnage*/
         void setPos(pos p);/*! \brief met à jour la position du personnage selon une position en paramètre \param p*/
         vector<float> getDir() const; /*! \brief renvoie la direction du personnage */
-        void setDir(vector<float> d); /*! \brief met à jour la direction selon un vecteur direction \param d en paramètre*/
         float getVit() const;/*! \brief renvoie la vitesse du personnage */
         void setVit(float v);/*! \brief met à jour la vitesse du personnage selon un paramètre vitesse \param v*/
         void deplacerD();/*! \brief déplace le personnage à droite */
         void deplacerG();/*! \brief déplace le personnage à gauche */
+        void saut();/*! \brief fait sauter le personnage, le déplace vers le haut*/
+        void tombe();/*! \brief fait tomber le personnage*/
         const string& getNom() const;/*! \brief renvoie le nom du personnage (permet de définir plusieurs apparences)*/
-        void setNom(const string nom);/*! \brief met à jour le nom du personnage selon un string \param nom en paramètre*/
-        bool estEnVie();/*! \brief renvoie true si en vie, false sinon*/
-
+        void setNom(const string nomP);/*! \brief met à jour le nom du personnage selon un string \param nom en paramètre*/
+        
 
 
 
