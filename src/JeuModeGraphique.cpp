783d369f21f3003ebb0ae0707b9e348239b21017
cyril.rigaut <cyril.rigaut@etu.univ-lyon1.fr>
2022-04-22 19:31:26 +0200

    initgraphique finie

--- src/JeuModeGraphique.cpp
+++ src/JeuModeGraphique.cpp
@@ -12,237 +12,69 @@
 const int TAILLE_SPRITE = 32;
 
 
-// ============= CLASS IMAGE =============== //
-
-Image::Image () : m_surface(nullptr), m_texture(nullptr), m_hasChanged(false) {
-}
-
-Image::~Image()
-{
-    SDL_FreeSurface(m_surface);
-    SDL_DestroyTexture(m_texture);
-
-    m_surface = nullptr;
-    m_texture = nullptr;
-    m_hasChanged = false;
-}
-
-
-/* void Image::loadFromFile (const char* filename, SDL_Renderer * renderer) {
-    m_surface = IMG_Load(filename);
-    if (m_surface == nullptr) {
-        string nfn = string("../") + filename;
-        cout << "Error: cannot load "<< filename <<". Trying "<<nfn<<endl;
-        m_surface = IMG_Load(nfn.c_str());
-        if (m_surface == nullptr) {
-            nfn = string("../") + nfn;
-            m_surface = IMG_Load(nfn.c_str());
-        }
-    }
-    if (m_surface == nullptr) {
-        cout<<"Error: cannot load "<< filename <<endl;
-        SDL_Quit();
-        exit(1);
-    }
-
-    SDL_Surface * surfaceCorrectPixelFormat = SDL_ConvertSurfaceFormat(m_surface,SDL_PIXELFORMAT_ARGB8888,0);
-    SDL_FreeSurface(m_surface);
-    m_surface = surfaceCorrectPixelFormat;
-
-    m_texture = SDL_CreateTextureFromSurface(renderer,surfaceCorrectPixelFormat);
-    if (m_texture == NULL) {
-        cout << "Error: problem to create the texture of "<< filename<< endl;
-        SDL_Quit();
-        exit(1);
-    }
-} */
-
-void Image::loadFromCurrentSurface (SDL_Renderer * renderer) {
-    m_texture = SDL_CreateTextureFromSurface(renderer,m_surface);
-    if (m_texture == nullptr) {
-        cout << "Error: problem to create the texture from surface " << endl;
-        SDL_Quit();
-        exit(1);
-    }
-}
-
-void Image::draw (SDL_Renderer * renderer, int x, int y, int w, int h) {
-    int ok;
-    SDL_Rect r;
-    r.x = x;
-    r.y = y;
-    r.w = (w<0)?m_surface->w:w;
-    r.h = (h<0)?m_surface->h:h;
-
-    if (m_hasChanged) {
-        ok = SDL_UpdateTexture(m_texture,nullptr,m_surface->pixels,m_surface->pitch);
-        assert(ok == 0);
-        m_hasChanged = false;
-    }
-
-    ok = SDL_RenderCopy(renderer,m_texture,nullptr,&r);
-    assert(ok == 0);
-}
-
-SDL_Texture * Image::getTexture() const {
-    return m_texture;
-    }
-
-void Image::setSurface(SDL_Surface * surf) {
-    m_surface = surf;
-    }
 
 // ============= CLASS JEUMODEGRAPHIQUE =============== //
 
 JeuModeGRAPHIQUE::JeuModeGRAPHIQUE():jeu() {
+}
+
+JeuModeGRAPHIQUE::~JeuModeGRAPHIQUE(){  
+}
+
+Vec2 JeuModeGRAPHIQUE::convertPos(Vec2 pos)
+{
+    Vec2 newPos;
+    double scaleX = 100 / DIMX;
+    // double scaleY = 12 / DIMY;
+    newPos.x = ((pos.x - cam.x) / scaleX + DIMX / 2);
+    newPos.y = (DIMY * pos.y) / 12;
+    //if (newPos.x >= 0 && newPos.x <= DIMX && newPos.y >=0 && newPos.y <= DIMY)
+        return newPos;
+}
+
+void JeuModeTXT::InitCam()
+{
+    cam.x = convertPos(jeu.getConstPersonnage().getPos()).x + DIMX / 2;
+    cam.y = convertPos(jeu.getConstPersonnage().getPos()).y + DIMY / 2;
+}
+
+
+void JeuModeGRAPHIQUE::affichageInitGRAPHIQUE(){
+    
+    int winx, winy;
+    Jeu jeu;
+
     // Initialisation de la SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() << endl;
         SDL_Quit();
         exit(1);
     }
-
+    // Initialisation de l'écriture
     if (TTF_Init() != 0) {
         cout << "Erreur lors de l'initialisation de la SDL_ttf : " << TTF_GetError() << endl;
         SDL_Quit();
         exit(1);
     }
 
-    /* int imgFlags = IMG_INIT_PNG | IMG_INIT_JPG;
-    if( !(IMG_Init(imgFlags) & imgFlags)) {
-        cout << "SDL_image could not initialize! SDL_image Error: " << IMG_GetError() << endl;
-        SDL_Quit();
-        exit(1);
-    }
-
-    if( Mix_OpenAudio( 44100, MIX_DEFAULT_FORMAT, 2, 2048 ) < 0 )
-    {
-        cout << "SDL_mixer could not initialize! SDL_mixer Error: " << Mix_GetError() << endl;
-        cout << "No sound !!!" << endl;
-        //SDL_Quit();exit(1);
-        withSound = false;
-    }
-    else withSound = true; */
-
-	int dimx, dimy;
-	dimx = dimx * TAILLE_SPRITE;
-	dimy = dimy * TAILLE_SPRITE;
-
     // Creation de la fenetre
-    window = SDL_CreateWindow("Foodle_Jump", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, dimx, dimy, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if (window == nullptr) {
+    window = SDL_CreateWindow("Foodle_Jump", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, winx*TAILLE_SPRITE, winy*TAILLE_SPRITE, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    if (window == NULL) {
         cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() << endl; 
         SDL_Quit(); 
         exit(1);
     }
 
+
+    //Initialisation du rendu
     renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
-
-    // IMAGES
-    /*im_perso.loadFromFile("data/perso.png",renderer);
-    im_projectile.loadFromFile("data/projectile.png",renderer);
-    im_bonus.loadFromFile("data/bonus.png",renderer);
-    im_monstres.loadFromFile("data/monstres.png",renderer);
-    
-    // FONTS
-    font = TTF_OpenFont("data/DejaVuSansCondensed.ttf",50);
-    if (font == nullptr)
-        font = TTF_OpenFont("../data/DejaVuSansCondensed.ttf",50);
-    if (font == nullptr) {
-            cout << "Failed to load DejaVuSansCondensed.ttf! SDL_TTF Error: " << TTF_GetError() << endl; 
-            SDL_Quit(); 
-            exit(1);
-	}
-	font_color.r = 50;font_color.g = 50;font_color.b = 255;
-	font_im.setSurface(TTF_RenderText_Solid(font,"Foodle_Jump",font_color));
-	font_im.loadFromCurrentSurface(renderer);
-
-    // SONS
-    /* if (withSound)
-    {
-        sound = Mix_LoadWAV("data/son.wav");
-        if (sound == nullptr) 
-            sound = Mix_LoadWAV("../data/son.wav");
-        if (sound == nullptr) {
-                cout << "Failed to load son.wav! SDL_mixer Error: " << Mix_GetError() << endl; 
-                SDL_Quit();
-                exit(1);
-        }
-    } */
-}
-
-
-JeuModeGRAPHIQUE::~JeuModeGRAPHIQUE(){
-    //if (withSound) Mix_Quit();
-    TTF_CloseFont(font);
-    TTF_Quit();
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-}
-
-
-
-
-void JeuModeGRAPHIQUE::affichageInitGRAPHIQUE(){
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-         cout << "Erreur lors de l'initialisation de la SDL : " << SDL_GetError() <<  endl;
-        SDL_Quit();
-        exit(1);
-    }
-
-    window = SDL_CreateWindow("Image", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 200, 200, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if (window == NULL) {
-         cout << "Erreur lors de la creation de la fenetre : " << SDL_GetError() <<  endl; 
-        SDL_Quit(); 
-        exit(1);
-    }
-    
-    renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
-
+   
 }
 
    
 
 
 
-/*void JeuModeGRAPHIQUE::boucleaffichageGRAPHIQUE(){
-    SDL_Event events;
-    bool quit=false;
-    // tant qu'il y a des évenements à traiter (cette boucle n'est pas bloquante)
-		while (SDL_PollEvent(&events)) {
-			if (events.type == SDL_QUIT) quit = true;           // Si l'utilisateur a clique sur la croix de fermeture
-			else if (events.type == SDL_KEYDOWN) {              // Si une touche est enfoncee
-                bool enVie = false;
-				switch (events.key.keysym.scancode) {
-				case SDL_SCANCODE_UP:
-					enVie = jeu.actionClavier('r');    // car Y inverse
-					break;
-				case SDL_SCANCODE_LEFT:
-					enVie = jeu.actionClavier('g');
-					break;
-				case SDL_SCANCODE_RIGHT:
-					enVie = jeu.actionClavier('d');
-					break;
-                case SDL_SCANCODE_ESCAPE:
-                case SDL_SCANCODE_Q:
-                    quit = true;
-                    break;
-				default: break;
-				}
-				if ((withSound) && (mangePastille))
-                    Mix_PlayChannel(-1,sound,0);
-			}
-		}
-
-		// on affiche le jeu sur le buffer cach�
-		affichageGRAPHIQUE();
-
-		// on permute les deux buffers (cette fonction ne doit se faire qu'une seule fois dans la boucle)
-        SDL_RenderPresent(renderer);
-	}
-} */
 
 
 
--- src/JeuModeGraphique.h
+++ src/JeuModeGraphique.h
@@ -12,26 +12,6 @@ using namespace std;
 #include <SDL_mixer.h>
 
 
-//! \brief Pour gérer une image avec SDL2
-class Image {
-
-private:
-
-    SDL_Surface *m_surface;
-    SDL_Texture *m_texture;
-    bool m_hasChanged;
-
-public:
-    Image () ;
-    ~Image();
-    void loadFromFile (const char* filename, SDL_Renderer * renderer);
-    void loadFromCurrentSurface (SDL_Renderer * renderer);
-    void draw (SDL_Renderer * renderer, int x, int y, int w=-1, int h=-1);
-    SDL_Texture * getTexture() const;
-    void setSurface(SDL_Surface * surf);
-};
-
-
 
 class JeuModeGRAPHIQUE
 {
@@ -45,12 +25,7 @@ private:
     SDL_Surface * background  = NULL ;
     SDL_Rect *rectangle;
     TTF_Font * font;
-    Image font_im;
     SDL_Color font_color;
-    Image im_projectile;
-    Image im_perso;
-    Image im_bonus;
-    Image im_monstres;
     bool souris;
     bool touche;
     Mix_Chunk * sound; 
@@ -61,12 +36,15 @@ private:
 public:
     JeuModeGRAPHIQUE();
     ~JeuModeGRAPHIQUE();
-    //void updatePlateau(Jeu &jeu); /*! \brief */
+    Vec2 convertPos(Vec2 pos);
+    void InitCam();
+    void updatePlateau(Jeu &jeu); /*! \brief */
     void affichageInitGRAPHIQUE(); /*! \brief affichage de SDL2*/
-    void affichageGRAPHIQUE(); /*! \brief affiche l'image dans la fenetre SDL2*/
-    void boucleaffichageGRAPHIQUE(); /*! \brief rafraichissement de l'image*/
-    void affDetruireGRAPHIQUE(); /*! \brief Détruit l'affichage*/
-
+    void affichageGRAPHIQUE(Jeu &jeu); /*! \brief affiche l'image dans la fenetre SDL2*/
+    void boucleAffGRAPHIQUE(Jeu &jeu, double dt); /*! \brief rafraichissement de l'image*/
+    void affDetruireGRAPHIQUE(Jeu &jeu); /*! \brief Détruit l'affichage*/
+    
+    
     
 };
 
